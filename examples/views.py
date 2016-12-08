a7b5f75566c960ccd5f982dfcb7f57f3c290390e
jaymollica <jmollica@sfmoma.org>
2016-12-08 14:51:05 -0800

    update to example views

--- examples/views.py
+++ examples/views.py
@@ -1,27 +1,25 @@
 from django.views.generic.base import View
 from django.http import HttpResponse
-from getty_getter import getty_getter
+from getty_getter import getty_getter as getty
 import json
-
-# Create your views here.
 
 class GetUlanView(View):
 	def get(self, request):
 
-		artist_ulan = getty_scraper.get_getty_ulan(u"Stieglitz, Alfred")
+		artist_ulan = json.dumps(getty.get_getty_ulan(u"Stieglitz, Alfred"))
 
 		return HttpResponse(artist_ulan, content_type="application/json")
 
 class GetArtistRelationshipsView(View):
 	def get(self, request):
 
-		artist_relationships = getty_scraper.get_getty_relationship("500024301")
+		artist_relationships = json.dumps(getty.get_getty_relationship("500024301"))
 
 		return HttpResponse(artist_relationships, content_type="application/json")
 
 class GetArtistNameView(View):
 	def get(self, request):
 
-		artist_relationships = getty_scraper.get_getty_artist_name("500024301")
+		artist_relationships = json.dumps(getty.get_getty_artist_name("500024301"))
 
 		return HttpResponse(artist_relationships, content_type="application/json")
